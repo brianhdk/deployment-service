f67a9715aee8afba90b91cbc7ad4d172f95ff8f4
brianhdk <bhk@yourhost.dk>
2016-11-23 12:34:52 +0100

    Vertica deployment service

---
+++ deployment/Package-Manager.ps1
@@ -0,0 +1,75 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$false)]
+	[ValidateSet("Local", "Remote")]
+	[string]$target = "Local"
+)
+
+$ErrorActionPreference = "Stop"
+$script_directory = Split-Path -Parent $PSCommandPath
+
+$settings = @{
+    "src" = @{
+        "deploymentservice" = Resolve-Path $script_directory\..\src\DeploymentService
+    }
+    "tools" = @{
+        "nuget" = Resolve-Path $script_directory\..\.nuget\NuGet.exe
+    }
+}
+
+# remove .nupkg files in script directory
+Get-ChildItem $script_directory | Where-Object { $_.Extension -eq ".nupkg" } | Remove-Item
+
+foreach ($project in $settings.src.Keys) {
+
+    $projectDirectory = Resolve-Path $settings.src[$project]
+
+    cd $projectDirectory
+
+    # remove .nupkg files in project directory
+    Get-ChildItem $projectDirectory | Where-Object { $_.Extension -eq ".nupkg" } | Remove-Item
+
+	# https://docs.nuget.org/create/nuspec-reference
+	$csproj = Get-ChildItem $projectDirectory | Where-Object { $_.Extension -eq ".csproj" }[0]
+
+    if ($csproj -eq $null) {
+        throw "No .csproj file found in $projectDirectory."
+    }
+
+	# https://docs.nuget.org/consume/command-line-reference
+    &$settings.tools.nuget pack $csproj -Build -Properties Configuration=Release -IncludeReferencedProjects -MSBuildVersion 14
+	
+    # Move .nupkg to script directory
+    Get-ChildItem $projectDirectory | Where-Object { $_.Extension -eq ".nupkg" } | Move-Item -Destination $script_directory -Force
+}
+
+
+Get-ChildItem $script_directory | Where-Object { $_.Extension -eq ".nupkg" } | ForEach {
+
+	If ($target -eq "Remote") {
+
+        Try {
+
+		    &$settings.tools.nuget push $_.FullName 66666666-6666-6666-6666-666666666666 -Source http://nuget.vertica.dk/api/v2/package
+        }
+        Catch {
+
+            $message = $_.Exception.Message
+            
+            If ($message.Contains("The server is configured to not allow overwriting packages that already exist.")) {
+
+                Write-Host "WARNING: $message" -ForegroundColor Yellow
+            }
+            Else {
+
+                Throw $_.Exception
+            }
+        }
+	} 
+    Else {
+
+		Get-ChildItem $projectDirectory | Where-Object { $_.Extension -eq ".nupkg" } | Move-Item -Destination "D:\Dropbox\Development\NuGet.Packages" -Force
+	}
+}
+
+cd $script_directory
--- src/DeploymentService/Properties/AssemblyInfo.cs
+++ src/DeploymentService/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeploymentService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Vertica Deployment Service")]
+[assembly: AssemblyDescription("Service to help out in certain deployment scenarios.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeploymentService")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Vertica A/S")]
+[assembly: AssemblyProduct("Deployment Service")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
